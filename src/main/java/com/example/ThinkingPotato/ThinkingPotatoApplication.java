c4ea692f776968fbf6b5c78409e6438c70ba7dc4
AVA030215 <avalee0215@gmail.com>
2024-12-06 17:51:36 +0900

    Development Environment for Backend

---
+++ src/main/java/com/example/ThinkingPotato/ThinkingPotatoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ThinkingPotato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkingPotatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkingPotatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ThinkingPotato/ThinkingPotatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThinkingPotato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkingPotatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
